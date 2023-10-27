5fd0a61de1bcc5bce76b83f8d3ca385db5ff3689
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2023-10-27 23:24:48 +0100

    Updated to use @apollo/server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
 const resolvers = require("./resolvers");
 const typeDefs = require("./schemas");
 const dotenv = require("dotenv");
@@ -6,17 +7,19 @@ const dotenv = require("dotenv");
 // Load environment variables from .env file
 dotenv.config();
 
-// Get the port number from the environment variables, or use 3000 as the default
-const port = process.env.PORT || 3000;
+async function startApolloServer() {
+    // Get the port number from the environment variables, or use 3000 as the default
+    const port = process.env.PORT || 3000;
 
-const server = new ApolloServer({ typeDefs, resolvers });
+    const server = new ApolloServer({ typeDefs, resolvers });
 
-server
-    .listen({ port })
-    .then(({ url }) => {
-        console.log(`Starting new Apollo Server at ${url} 🚀`);
-    })
-    .catch((error) => {
+    try {
+        const { url } = await startStandaloneServer(server, {
+            context: async ({ req }) => ({ token: req.headers.token }),
+            listen: { port },
+        });
+        console.log(`🚀  Server ready at ${url}`);
+    } catch (error) {
         if (error.code === "EADDRINUSE") {
             console.error(
                 `Port ${port} is already in use. Please choose another port.`
@@ -24,4 +27,7 @@ server
         } else {
             console.error(`Error starting Apollo Server: ${error}`);
         }
-    });
+    }
+}
+
+startApolloServer();

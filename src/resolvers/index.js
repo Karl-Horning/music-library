eb2c083f5185fd23af7942c5577d47d43e7d0a9c
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2023-10-29 19:51:47 +0000

    Added routes and schema to create and delete an artist

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,6 +1,44 @@
+const { v4: uuidv4 } = require("uuid");
+
 const resolvers = {
     Query: {
         hello: () => "Hello!",
+
+        artists: async (_, __, { prisma }) =>
+            prisma.artist.findMany({
+                orderBy: {
+                    name: "asc",
+                },
+            }),
+    },
+    Mutation: {
+        createArtist: async (_, { name }, { prisma }) => {
+            try {
+                const artist = await prisma.artist.create({
+                    data: {
+                        id: uuidv4(),
+                        name,
+                    },
+                });
+                return artist;
+            } catch (error) {
+                console.error("Error creating artist:", error);
+                throw new Error("Failed to create artist");
+            }
+        },
+        deleteArtist: async (_, { id }, { prisma }) => {
+            try {
+                const artist = await prisma.artist.delete({
+                    where: {
+                        id,
+                    },
+                });
+                return artist;
+            } catch (error) {
+                console.error("Error deleting artist:", error);
+                throw new Error("Failed to delete artist");
+            }
+        },
     },
 };
 
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,6 +3,26 @@ const { gql } = require("apollo-server");
 const typeDefs = gql`
     type Query {
         hello: String
+        artists: [Artist!]!
+    }
+
+    type Mutation {
+        createArtist(name: String!): Artist!
+        deleteArtist(id: ID!): Artist!
+    }
+
+    type Artist {
+        id: ID!
+        name: String!
+        albums: [Album!]!
+    }
+
+    type Album {
+        id: ID!
+        title: String!
+        year: Int!
+        artistId: ID!
+        artist(id: ID): Artist
     }
 `;
 

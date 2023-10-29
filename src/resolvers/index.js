6becc15f80ca4119246cd2e05df5c97457b66212
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2023-10-29 22:40:43 +0000

    Added more routes

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -10,8 +10,17 @@ const resolvers = {
                     name: "asc",
                 },
             }),
+
         artist: async (_, { id }, { prisma }) => {
             return prisma.artist.findUnique({
+                where: {
+                    id,
+                },
+            });
+        },
+
+        album: (_, { id }, { prisma }) => {
+            return prisma.album.findUnique({
                 where: {
                     id,
                 },
@@ -33,6 +42,7 @@ const resolvers = {
                 throw new Error("Failed to create artist");
             }
         },
+
         deleteArtist: async (_, { id }, { prisma }) => {
             try {
                 const artist = await prisma.artist.delete({
@@ -45,6 +55,86 @@ const resolvers = {
                 console.error("Error deleting artist:", error);
                 throw new Error("Failed to delete artist");
             }
+        },
+
+        createAlbum: async (_, { title, year, artistId }, { prisma }) => {
+            try {
+                const album = await prisma.album.create({
+                    data: {
+                        id: uuidv4(),
+                        title,
+                        year,
+                        artistId,
+                    },
+                });
+                return album;
+            } catch (error) {
+                console.error("Error creating album:", error);
+                throw new Error("Failed to create album");
+            }
+        },
+
+        updateArtist: async (_, { id, name }, { prisma }) => {
+            try {
+                const artist = await prisma.artist.update({
+                    where: {
+                        id,
+                    },
+                    data: {
+                        name,
+                    },
+                });
+                return artist;
+            } catch (error) {
+                console.error("Error updating artist:", error);
+                throw new Error("Failed to update artist");
+            }
+        },
+
+        updateAlbum: async (_, { id, title, year, artistId }, { prisma }) => {
+            if (title || year || artistId) {
+                try {
+                    const album = await prisma.album.update({
+                        where: {
+                            id,
+                        },
+                        data: {
+                            title,
+                            year,
+                            artistId,
+                        },
+                    });
+                    return album;
+                } catch (error) {
+                    console.error("Error updating album:", error);
+                    throw new Error("Failed to update album");
+                }
+            } else {
+                throw new Error("Enter either a new title, year, or artistId");
+            }
+        },
+    },
+
+    Artist: {
+        albums: ({ id }, __, { prisma }) => {
+            return prisma.album.findMany({
+                where: {
+                    artistId: id,
+                },
+                orderBy: {
+                    year: "asc",
+                },
+            });
+        },
+    },
+
+    Album: {
+        artist: ({ artistId }, _, { prisma }) => {
+            return prisma.artist.findUnique({
+                where: {
+                    id: artistId,
+                },
+            });
         },
     },
 };
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -5,11 +5,24 @@ const typeDefs = gql`
         hello: String
         artists: [Artist!]!
         artist(id: ID!): Artist!
+        album(id: ID!): Album
     }
 
     type Mutation {
         createArtist(name: String!): Artist!
         deleteArtist(id: ID!): Artist!
+        createAlbum(
+            title: String!
+            year: Int!
+            artistId: ID!
+        ): Album!
+        updateArtist(id: ID!, name: String!): Artist!
+        updateAlbum(
+            id: ID!
+            title: String
+            year: Int
+            artistId: String
+        ): Album!
     }
 
     type Artist {

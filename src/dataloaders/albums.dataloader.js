cb96d9b607a28c37a4bb5330a327f53091fced0b
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2023-11-09 21:14:11 +0000

    Added JSDoc

--- src/dataloaders/albums.dataloader.js
+++ src/dataloaders/albums.dataloader.js
@@ -1,3 +1,7 @@
+/**
+ * @module albumsDataLoader
+ */
+
 const DataLoader = require("dataloader");
 const { groupBy, map } = require("ramda");
 const { PrismaClient } = require("@prisma/client");
@@ -6,6 +10,13 @@ const prisma = new PrismaClient({
     log: ["query"], // Enable query logging
 });
 
+/**
+ * Data loader function that loads albums by artist IDs using Prisma.
+ * @function
+ * @param {number[]} artistIds - An array of artist IDs.
+ * @returns {Promise<Object>} A promise that resolves to an object where keys are artist IDs and values are arrays of albums.
+ * @throws {Error} If there is an error while fetching albums.
+ */
 const getAlbumsByArtistIds = (artistIds) => {
     return prisma.album
         .findMany({
@@ -24,6 +35,11 @@ const getAlbumsByArtistIds = (artistIds) => {
         });
 };
 
+/**
+ * Creates a new DataLoader for fetching albums by artist IDs.
+ * @function
+ * @returns {DataLoader} A DataLoader instance for loading albums.
+ */
 const albumsDataLoader = () => {
     return new DataLoader(getAlbumsByArtistIds);
 };
